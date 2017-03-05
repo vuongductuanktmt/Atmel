300c0616fa9a6c08027bff9ffb783141861c21c9
Vuong Duc Tuan <anhtuanpro312@gmail.com>
2017-03-05 23:23:52 +0700

    remove folder LOCK_DOOR

--- 8051/LOCK_DOOR/CODE/24cxx/24cxx.h
+++
@@ -1,9 +0,0 @@
-#ifndef __24CXX_H
-#define __24CXX_H
-#include "main.h"
-void EEP_24CXX_Init();
-unsigned char EEP_24CXX_Read(unsigned char address);
-void EEP_24CXX_Write(unsigned char address,unsigned char Data);
-void EEP_24CXX_WriteS(unsigned char address,unsigned char *s);
-void EEPROM_24CXX_ReadS(unsigned char address, unsigned char lenght, unsigned char *s);
-#endif
